cc05622f92b25d8f552fa4f803dea8738cdefa18
tobzman <127453486+tobzman@users.noreply.github.com>
2023-10-09 14:47:47 +0100

    updated userRoutes.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { User, Blog } = require("../../models");
+const withAuth = require('../../utils/auth'); // Adjust the path as needed
 
 router.get("/", async (req, res) => {
   try {
